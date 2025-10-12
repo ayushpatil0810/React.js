2ddb5572ad0053b9e7466a1e3bef4e2069cba1b8
Ayush Patil <94798136+ayushpatil0810@users.noreply.github.com>
2025-10-13 01:17:38 +0530

    edited file

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState, useCallback, useEffect, useRef } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -16,15 +14,15 @@ function App() {
     let pass = ''
     let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz'
 
-    if(numberAllowed) str += '0123456789'
-    if(charAllowed) str += '~!@#$%^&*()-_+={}[]|\:<>,.?/'
+    if (numberAllowed) str += '0123456789'
+    if (charAllowed) str += '~!@#$%^&*()-_+={}[]|\:<>,.?/'
 
     for (let i = 1; i < length; i++) {
       const char = Math.floor(Math.random() * str.length + 1)
       pass += str.charAt(char)
 
       setPassword(pass)
-      
+
     }
 
   }, [length, numberAllowed, charAllowed])
@@ -35,31 +33,40 @@ function App() {
   }
 
 
-  useEffect(()=> {
+  useEffect(() => {
     generatePassword()
   }, [length, numberAllowed, charAllowed])
 
   return (
     <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
+
       <h1 className='text-white text-center my-3'>Password Generator</h1>
+
       <div className='flex shadow rounded-lg overflow-hidden mb-4'>
-        <input type="text" value={password} className='outline-none w-full py-1 px-3 bg-white' readOnly placeholder='Password' ref={passwordRef}/>
+        <input type="text" value={password} className='outline-none w-full py-1 px-3 bg-white' readOnly placeholder='Password' ref={passwordRef} />
         <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 cursor-pointer' onClick={copyPasswordToClipboard}>Copy</button>
       </div>
+
       <div className='flex text-sm gap-x-2'>
+
         <div className='flex items-center gap-x-1'>
           <input type="range" min={6} max={100} value={length} className='cursor-pointer' onChange={(e) => setLength(e.target.value)} name='' id='' />
           <label htmlFor="length">Length: {length}</label>
+
           <div className='flex items-center gap-x-1'>
-            <input type='checkbox'  defaultChecked={numberAllowed} onChange={() => {setNumberAllowed((prevValue) => !prevValue)}} name='' id=''></input>
+            <input type='checkbox' defaultChecked={numberAllowed} onChange={() => { setNumberAllowed((prevValue) => !prevValue) }} name='' id=''></input>
             <label htmlFor="number">Numbers</label>
           </div>
+
           <div className='flex items-center gap-x-1'>
-            <input type='checkbox'  defaultChecked={charAllowed} onChange={() => {setCharAllowed((prevValue) => !prevValue)}} name='' id=''></input>
+            <input type='checkbox' defaultChecked={charAllowed} onChange={() => { setCharAllowed((prevValue) => !prevValue) }} name='' id=''></input>
             <label htmlFor="charInput">Characters</label>
           </div>
+
         </div>
+
       </div>
+
     </div>
   )
 }
